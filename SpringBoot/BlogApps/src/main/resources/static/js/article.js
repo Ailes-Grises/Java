5e93512e1653f07ec49ba27c0e6193c4d14e6fcf
Ailes-Grises <toriniku.n.s@gmail.com>
2021-07-10 01:23:29 +0900

    feature: BlogApps のREADME.md 用に素材置き場のブランチを切った。

--- SpringBoot/BlogApps/src/main/resources/static/js/article.js
+++
@@ -1,9 +0,0 @@
-// th:onclick="setIsFav([[${article.id}]]);" deyobidasi
-console.log("test");
-window.onload = ()=> {
-    // alert("未fav");
-    // if(isFav()){
-    //     alert("fav済");
-    // }
-    
-};
